3644899ff21e4a2b110324e3557fe9cb247d4f47
FacundoLastra <facundolastra94@gmail.com>
2018-12-07 12:00:01 -0300

    added library for video playback
    and moved camera bottons for another component

---
+++ app/components/MyListVideo.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+import {StyleSheet, View, Dimensions} from 'react-native';
+
+import Video from 'react-native-video';
+
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
+export default class MyListVideo extends PureComponent {
+    constructor(props){
+      super(props)
+    }  
+    
+    render() {
+      return (
+          <View style={styles.container}>
+          
+            <Video source={{uri: this.props.video.uri}}  
+                ref={(ref) => {
+                    this.player = ref
+                }}
+                paused={false}                             
+                onBuffer={this.onBuffer}                
+                onError={this.videoError}               
+                style={styles.backgroundVideo}
+             />
+
+          </View>
+      );
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,    
+      marginTop: 5,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    backgroundVideo: {
+        position: 'absolute',
+        top: 10,
+        left: 10,
+        bottom: 10,
+        right: 10
+      }
+  
+  })
--- app/views/Gallery.js
+++ app/views/Gallery.js
@@ -1,35 +1,71 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 import { connect } from 'react-redux';
 import MyListPhoto from '../components/MyListPhoto'
-//import Icon from 'react-native-vector-icons/EvilIcons'
+import MyListVideo from '../components/MyListVideo'
 
+//import Icon from 'react-native-vector-icons/EvilIcons'
 import Icon from 'react-native-vector-icons/FontAwesome';
-const myIcon = (<Icon name="rocket" size={30} color="#900" />)
+const myIcon = (<Icon name="image" size={30} color="#900" />)
 
 
  class Gallery extends React.Component {
     constructor(props){
         super(props)
-        this.renderItem = this.renderItem.bind(this);
+        this.renderItemImages = this.renderItemImages.bind(this);
+        this.renderItemVideos = this.renderItemVideos.bind(this);
+        this.state = {
+            viewSelected: 'images'
+        }
     } 
 
-    renderItem(item) {
+    renderItemImages(item) {
         return (
             <MyListPhoto
                 photo= {item.item.photo}
             />
         )
+    }
 
+    renderItemVideos(item) {
+        return(
+            <MyListVideo
+                video = {item.item.video}
+            />
+        )
     }
     render() {
+        let listView = <View/>
+        if(this.state.viewSelected === 'images'){
+            listView = <FlatList
+                        data={this.props.photos}
+                        keyExtractor={ (item) => item.id.toString()}
+                        renderItem={this.renderItemImages}
+                     />
+        }else{
+            listView = <MyListVideo    ///bug using flatList and ScrolView
+                        video = {this.props.videos[0].video}
+                        />   
+        }
         return (
-            <View>
-               <FlatList
-                    data={this.props.photos}
-                    keyExtractor={ (item) => item.id.toString()}
-                    renderItem={this.renderItem}
-      />
+            <View style={styles.container}>
+                <View style={styles.bottonsContainer}>
+                    <TouchableOpacity
+                        style={[styles.flipButton]}
+                        onPress={() => this.setState({viewSelected: "images"})}
+                    >
+                        <Icon name="image" size={40} color="#900" />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.flipButton]}
+                        onPress={() => this.setState({viewSelected: "videos"})}
+                    >
+                        <Icon name="file-video-o" size={40} color="#900" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.listContainer}>
+                     {listView}        
+                </View>               
             </View>
         )
     }
@@ -43,3 +79,25 @@ function mapStateToProps(state, props) {
 }
 //Connect everything
 export default connect(mapStateToProps)(Gallery);
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1
+    },
+    bottonsContainer: {
+        flex: 0.1,
+        marginBottom: 3,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    listContainer: {
+        flex: 0.9
+    },
+    flipButton: {
+        flex: 0.2,
+        height: 40,
+        marginHorizontal: 2,
+        marginBottom: 10,
+        padding: 5
+    }
+})
